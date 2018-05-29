3d4153656cf0cfea9a9639b426117e2d5f6cda88
Keith <keithwhisper@gmail.com>
2018-05-29 10:16:00 +0800

    update: formalize transaction list

--- lib/agera_one_web/views/transaction_view.ex
+++ lib/agera_one_web/views/transaction_view.ex
@@ -9,19 +9,17 @@ defmodule AgeraOneWeb.TransactionView do
   end
 
   def render("index.json", %{transactions: transactions}) do
-    %{data: render_many(transactions, TransactionView, "transaction.json")}
+    %{result: render_many(transactions, TransactionView, "transaction.json")}
   end
 
   def render("show.json", %{transaction: transaction}) do
-    %{data: render_one(transaction, TransactionView, "transaction.json")}
+    %{result: render_one(transaction, TransactionView, "transaction.json")}
   end
 
   def render("transaction.json", %{transaction: transaction}) do
     %{
-      result: %{
-        hash: transaction.hash,
-        content: transaction.content
-      }
+      hash: transaction.hash,
+      content: transaction.content
     }
   end
 
